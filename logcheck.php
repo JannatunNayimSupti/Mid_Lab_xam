1df30ac8a550b2707e38da52ce7ea6a50b0b1141
JannatunNayimSupti <67923666+JannatunNayimSupti@users.noreply.github.com>
2020-08-10 13:44:55 +0600

    Add files via upload

--- logcheck.php
+++ logcheck.php
@@ -3,13 +3,14 @@
 if(isset($_POST['submit'])){
     $conn = mysqli_connect('localhost', 'root', '', 'webtechLab');
 
-        if(empty($_POST['userId']) || empty($_POST['pass'])){
+        if(empty($_POST['userId']) || empty($_POST['pass'])|| empty($_POST['userType'])){
             echo "Invalid";
         }
         else{
             $userId = $_POST['userId'];
+             $userType = $_POST['userType'];
             $password = $_POST['pass'];
-            $query = "SELECT * FROM userinfo WHERE userId = '$userId' AND Pass = '$password'";
+            $query = "SELECT * FROM userinfo WHERE userId = '$userId', userType = '$userType' AND pass = '$password'";
             $result = mysqli_query($conn, $query);
 
             while( $row = mysqli_fetch_assoc($result) ){
@@ -20,35 +21,35 @@ if(isset($_POST['submit'])){
                 $Cpassword = $row['password'];
                 $CuserType = $row['userType'];
 
-                if(($uname == $Cusername) && ($password == $Cpassword)){
+                if(($userId == $userId) && ($password == $password)){
                     if($CuserType == 'Admin'){
 
-                    	$user = ['name'=> $Cname,'userId'=> $Cusername,'userType'=> $CuserType,'pass'=> $Cpassword];
+                    	$user = ['name'=> $name,'userId'=> $userId,'userType'=> $userType,'pass'=> $Cpassword];
 
 			
-			            $_SESSION['name']   = $uname;
-			            $_SESSION['userName']   = $uname;
-			            $_SESSION['userType'] 	= $CuserType;
+			            $_SESSION['name']   = $name;
+			            $_SESSION['userId']    = $userId;
+                         $_SESSION['email']    = $email;
+			            $_SESSION['userType'] 	= $userType;
 			            $_SESSION['pass'] 	    = $password;
 			            $_SESSION['user'] 		= $user;
 
 			
 			            setcookie('name', $name, time()+3600, '/');
-			            setcookie('userName', $uname, time()+3600, '/');
+                        setcookie('userType', $userType, time()+3600, '/');
+			            setcookie('userId', $userId, time()+3600, '/');
 			            setcookie('email', $email, time()+3600, '/');
 			            setcookie('pass', $password, time()+3600, '/');
+                        
 			            
 
                         header('location: AdminPage.php');
                     }
-                    else if($CuserType == 'User'){
+
+                    else ($CuserType == 'User'){
                         header('location: UserPage.php');
                     }
                 }
-            } 
-        }
-    }  
-else
-{
-    echo "sorry!!";
-}
+            }
+
+?>
